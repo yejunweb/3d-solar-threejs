2ef1d85390545737ee07553d2fa25f20f69f1990
yejun <2512636001@qq.com>
2025-04-11 15:26:25 +0800

    chore: change server port

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-const { VantResolver } = require('@vant/auto-import-resolver')
-const ComponentsPlugin = require('unplugin-vue-components/webpack')
+const { defineConfig } = require('@vue/cli-service');
+const { VantResolver } = require('@vant/auto-import-resolver');
+const ComponentsPlugin = require('unplugin-vue-components/webpack');
 module.exports = defineConfig({
-  publicPath: './',
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      ComponentsPlugin({
-        resolvers: [VantResolver()],
-      }),
-    ],
-  },
-})
+    publicPath: './',
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            ComponentsPlugin({
+                resolvers: [VantResolver()],
+            }),
+        ],
+    },
+    devServer: {
+        port: 8888,
+    },
+});
